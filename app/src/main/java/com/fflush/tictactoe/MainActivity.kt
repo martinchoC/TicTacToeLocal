8ebcbc1ddb25922055e95773a44e6acc3c146acf
Martin <martin.e.castro.88@gmail.com>
2017-10-31 00:14:48 -0300

    OnClick function created. When someone wins, buttons are disabled. An icon for the app was set

--- app/src/main/java/com/fflush/tictactoe/MainActivity.kt
+++ app/src/main/java/com/fflush/tictactoe/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.fflush.tictactoe
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,7 +29,104 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> cellID = 8
             R.id.button9 -> cellID = 9
         }
+        //Toast.makeText(this, "ID: $cellID", Toast.LENGTH_LONG).show()
+        playGame(cellID, buttonSelected)
+    }
 
-        Toast.makeText(this, "ID: $cellID", Toast.LENGTH_LONG).show()
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+    var activePlayer = 1
+
+    fun playGame(cellID: Int, buttonSelected: Button){
+
+        if(activePlayer == 1){
+            buttonSelected.text = "X"
+            buttonSelected.setBackgroundColor(Color.GREEN)
+            player1.add(cellID)
+            activePlayer = 2
+        }
+        else{
+            buttonSelected.text = "O"
+            buttonSelected.setBackgroundColor(Color.RED)
+            player2.add(cellID)
+            activePlayer = 1
+        }
+        buttonSelected.isEnabled = false
+        checkWinner()
+    }
+
+    fun checkWinner(){
+        var winner = -1
+
+        //row 1
+        if(player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+            winner = 1
+        }
+        if(player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+            winner = 2
+        }
+
+        //row 2
+        if(player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+            winner = 1
+        }
+        if(player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+            winner = 2
+        }
+
+        //row 3
+        if(player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+            winner = 1
+        }
+        if(player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+            winner = 2
+        }
+
+        //col 1
+        if(player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+            winner = 1
+        }
+        if(player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+            winner = 2
+        }
+
+        //col 2
+        if(player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+            winner = 1
+        }
+        if(player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+            winner = 2
+        }
+
+        //col 3
+        if(player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+            winner = 1
+        }
+        if(player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+            winner = 2
+        }
+
+        if( winner != -1){
+            if(winner == 1){
+                Toast.makeText(this, "PLAYER 1 WINS", Toast.LENGTH_LONG).show()
+                disableButtons()
+            }
+            else{
+                Toast.makeText(this, "PLAYER 2 WINS", Toast.LENGTH_LONG).show()
+                disableButtons()
+            }
+        }
+    }
+
+    fun disableButtons(){
+        button1.isEnabled = false
+        button2.isEnabled = false
+        button3.isEnabled = false
+        button4.isEnabled = false
+        button5.isEnabled = false
+        button6.isEnabled = false
+        button7.isEnabled = false
+        button8.isEnabled = false
+        button9.isEnabled = false
     }
 }
